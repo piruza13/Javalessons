57a32b695f2f64f7c6536af083c229b97c9e72ff
piruza <piruza.sargsyan@buymie.eu>
2023-09-08 11:09:06 +0100

    the second one

---
+++ src/main/java/Reverse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int rev = 0;
+        while (number != 0)
+        { rev = rev*10 + number%10;
+          number = number/10;
+
+        }
+        System.out.println ("The reverse number is: " + rev);
+    }
+}
